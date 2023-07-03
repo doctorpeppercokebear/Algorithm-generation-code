58a0b5bf7a13de91d7eab2958723f92aa5e7daab
cokeicebear <river98@hanmail.net>
2023-07-03 14:07:38 +0900

    React 연습

--- 12.배추/backend/ai/views.py
+++ 12.배추/backend/ai/views.py
@@ -11,11 +11,12 @@ import numpy as np
 def ai(request):
     temperature = float(request.GET['temperature'])
     humidity = float(request.GET['humidity'])
-    wind_speed = float(request.GET['wind_speed'])
-    
+    # wind_speed = float(request.GET['wind_speed'])
+
+    # 평균 온도 습도를 가지고 학습된 모델
     with open('ai/bechu.pkl', 'rb') as fp:
         model = pickle.load(fp)
-    predict = model.predict([[humidity, wind_speed]]) 
+    predict = model.predict([[temperature, humidity]]) 
 
     return Response([{'ai': '모민규'},
                     # {'temperature' : temperature},
